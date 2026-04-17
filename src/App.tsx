a26e8b4c2c785f2a03b19aac82cbabacf9de31a4
MvLeronn <marcos23_correia@hotmail.com>
2026-04-16 23:51:55 -0300

    feat: add AppRoutes with typed navigation stack

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import { View } from 'react-native';
-import { Home } from './pages/Home';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
 import { useEffect } from 'react';
+import { AppRoutes } from './AppRoutes';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -23,9 +22,5 @@ export function App() {
     return null;
   }
 
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Home />
-    </View>
-  );
+  return <AppRoutes />
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,17 @@
-import { View, Text } from "react-native"
+import { useNavigation } from "@react-navigation/native"
+import { View, Text, TouchableOpacity } from "react-native"
+import { TNavigationScreenProps } from "../AppRoutes";
 
 export const Home = () => {
+    const navigation = useNavigation<TNavigationScreenProps>();
+
     return (
         <View>
             <Text style={{ fontSize: 30, fontFamily: 'InterRegular' }}>Home</Text>
+
+            <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
+                <Text style={{ fontSize: 30, fontFamily: 'InterRegular' }}>Settings</Text>
+            </TouchableOpacity>
         </View>
     )
 }
